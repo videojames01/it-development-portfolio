0371f595600665910528f915e1cb7542630738b9
James Openshaw <open0008@hz.nl>
2025-06-13 13:41:27 +0200

    working Laravel Breeze register and login

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Models\Task;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index')->middleware(['auth']);
+Route::resource('tasks', TaskController::class)->except('index')->middleware(['auth']);
 
-Route::resource('tasks', TaskController::class)
-    ->except('index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Error routes
-Route::get('/server-error', function () {
-    abort(500);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
